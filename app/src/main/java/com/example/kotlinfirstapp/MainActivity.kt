389b6b222ad8a264f9653a2030943ed59ec68b96
RafidKhan <rafidkhan321.rk@gmail.com>
2021-01-21 10:37:46 +0600

    First Kotlin Project pt2

--- app/src/main/java/com/example/kotlinfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfirstapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinfirstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -18,6 +19,16 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Clicked!", Toast.LENGTH_SHORT).show()
 
 
+            btnSendMsgToNextActivity.setOnClickListener {
+                var message: String = etUserMessage.text.toString().trim()
+                Toast.makeText(this, "Message: "+message, Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, SecondActivity::class.java)
+
+                startActivity(intent)
+
+
+            }
 
         }
     }
